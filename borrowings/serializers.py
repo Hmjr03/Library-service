d6a9398ec97f687243bbf0931b0aef2cfda4f113
Humberto Manhani Junior <hmanhani@hotmail.com>
2026-09-15 01:01:14 +0200

    feat(borrowings): create loans with atomic inventory updates

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,6 +1,10 @@
+from django.utils import timezone
 from rest_framework import serializers
+
 from books.serializers import BookSerializer
+
 from .models import Borrowing
+from .services import create_borrowing
 
 
 class BorrowingReadSerializer(serializers.ModelSerializer):
@@ -10,3 +14,19 @@ class BorrowingReadSerializer(serializers.ModelSerializer):
         model = Borrowing
         fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user")
         read_only_fields = fields
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book", "expected_return_date")
+
+    def validate_expected_return_date(self, value):
+        if value < timezone.localdate():
+            raise serializers.ValidationError("Return date cannot be before today.")
+        return value
+
+    def create(self, validated_data):
+        return create_borrowing(user=self.context["request"].user, **validated_data)
+
+
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,12 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import mixins, status, viewsets
+from rest_framework.response import Response
 from .models import Borrowing
-from .serializers import BorrowingReadSerializer
+from .serializers import BorrowingCreateSerializer, BorrowingReadSerializer
 
 
-class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+class BorrowingViewSet(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Borrowing.objects.select_related("book", "user").all()
     serializer_class = BorrowingReadSerializer
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset if self.request.user.is_staff else queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        return BorrowingCreateSerializer if self.action == "create" else BorrowingReadSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(BorrowingReadSerializer(serializer.save()).data, status=status.HTTP_201_CREATED)
