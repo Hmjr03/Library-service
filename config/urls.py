6fd3885d0f8b94d1fbb73418ca415e10446cc5b2
Humberto Manhani Junior <hmanhani@hotmail.com>
2026-09-15 01:01:14 +0200

    feat(books): implement inventory model and CRUD endpoints

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,7 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from books.views import BookViewSet
 
-urlpatterns = [path("users/", include("users.urls"))]
+router = DefaultRouter()
+router.register("books", BookViewSet)
+urlpatterns = [path("users/", include("users.urls")), path("", include(router.urls))]
